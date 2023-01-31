807e3592658f358e31e727fdc50d3cf609a8502d
MrVanDaime <mrvandaime@gmail.com>
2023-01-31 08:57:36 -0300

    Alterada a forma de guardar o resultado do getJSON e foram adicionados comentários

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,50 +1,59 @@
 jQuery( document ).ready( function( $ ) {
 	const json_file = "data.json";
 
-	let data_json;
-	$.getJSON( json_file, function( json ) {
-		data_json = json;
-	});
-
-	setTimeout(() => {
-		const btn_gerar = $( "#btn_gerar" );
-		const accordion = $( "#accordionMain" );
-		$( "#qtd_perguntas" ).attr( "min", 1 );
-		$( "#qtd_perguntas" ).attr( "max", data_json.length );
-
-		btn_gerar.on( "click", function(){
-			let qtd_perguntas = $( "#qtd_perguntas" ).val();
-
-			if ( data_json.length >= qtd_perguntas && qtd_perguntas > 0 ) {
-				var arr_selecionados = [];
-				let html = '';
-				while( arr_selecionados.length < qtd_perguntas ){
-					var index = Math.floor( Math.random() * data_json.length );
-					if ( !arr_selecionados.includes( data_json[index] ) ) {
-						let dificuldade = data_json[index]["dificuldade"];
-						switch ( dificuldade ) {
-							case 'facil':
-								dificuldade = 'success';
-							break;
-							case 'medio':
-								dificuldade = 'warning';
-							break;
-							case 'dificil':
-								dificuldade = 'danger';
-							break;
-						}
-
-						arr_selecionados.push( data_json[index] );
-						html += '<div class="accordion-item bg-'+dificuldade+'"><h2 class="accordion-header" id="heading'+data_json[index]["id"]+'"><button class="accordion-button collapsed bg-'+dificuldade+'" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+data_json[index]["id"]+'" aria-expanded="false" aria-controls="collapse'+data_json[index]["id"]+'"><b>'+ data_json[index]["pergunta"] +'</b></button></h2>'+
-							'<div id="collapse'+data_json[index]["id"]+'" class="accordion-collapse collapse" aria-labelledby="heading'+data_json[index]["pergunta"]+'" data-bs-parent="#accordionMain"><div class="accordion-body">'+data_json[index]["resposta"]+'</div></div></div>';
-					}
-				}
-				accordion.html( '' );
-				accordion.append( html );
-			} else {
-				alert( "Mano?? Preenche isso aí corretamente!" );
-			}
+	let json_data = [];
+	// Guarda o resultado em localStorage
+	if ( localStorage.json_data ) {
+		$.getJSON( json_file, function( json ) {
+			localStorage.json_data = JSON.stringify( json );
 		});
-	}, 500);
+	}
+
+	// Transforma em array
+	json_data = JSON.parse( localStorage.json_data ); 
+
+	const btn_gerar = $( "#btn_gerar" );
+	const accordion = $( "#accordionMain" );
+
+	// Valor mínimo e máximo do campo de quantidade de perguntas
+	$( "#qtd_perguntas" ).attr( "min", 1 );
+	$( "#qtd_perguntas" ).attr( "max", json_data.length );
+
+	btn_gerar.on( "click", function(){
+		let qtd_perguntas = $( "#qtd_perguntas" ).val();
+
+		if ( json_data.length >= qtd_perguntas && qtd_perguntas > 0 ) {
+			var arr_selecionados = [];
+			let html = '';
+			while( arr_selecionados.length < qtd_perguntas ){
+				var index = Math.floor( Math.random() * json_data.length );
+				if ( !arr_selecionados.includes( json_data[index] ) ) {
+					let dificuldade = json_data[index]["dificuldade"];
+					switch ( dificuldade ) {
+						case 'facil':
+							dificuldade = 'success';
+						break;
+						case 'medio':
+							dificuldade = 'warning';
+						break;
+						case 'dificil':
+							dificuldade = 'danger';
+						break;
+					}
+
+					// Guarda o index para não duplicar depois
+					arr_selecionados.push( json_data[index] );
+
+					html += '<div class="accordion-item bg-'+dificuldade+'"><h2 class="accordion-header" id="heading'+json_data[index]["id"]+'"><button class="accordion-button collapsed bg-'+dificuldade+'" type="button" data-bs-toggle="collapse" data-bs-target="#collapse'+json_data[index]["id"]+'" aria-expanded="false" aria-controls="collapse'+json_data[index]["id"]+'"><b>'+ json_data[index]["pergunta"] +'</b></button></h2>'+
+						'<div id="collapse'+json_data[index]["id"]+'" class="accordion-collapse collapse" aria-labelledby="heading'+json_data[index]["pergunta"]+'" data-bs-parent="#accordionMain"><div class="accordion-body">'+json_data[index]["resposta"]+'</div></div></div>';
+				}
+			}
+			accordion.html( '' );
+			accordion.append( html );
+		} else {
+			// Caso o campo não for preenchido corretamente
+			alert( "Mano?? Preenche isso aí corretamente!" );
+		}
+	});
 
 });
